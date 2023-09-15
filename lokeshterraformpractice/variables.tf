221281e0dc89fc3e411b38d90d52df12f44aa23f
lokesh devops <97424924+loky1997@users.noreply.github.com>
2023-09-15 11:40:37 +0530

    Create variables.tf

---
+++ lokeshterraformpractice/variables.tf
@@ -0,0 +1,12 @@
+variable "ami_value" {
+        description = "value for the ami"
+}
+variable "instance_type_value" {
+        description = "value for the instance type"
+}
+variable "subnet_id_value" {
+        description = "value for subnet ID"
+}
+variable "key_name_value" {
+        description = "keypair value"
+}
